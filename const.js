74f4ffc65a65c948f6e619c6983f7339229c23fe
Hamza <hamza.elmk@gmail.com>
2017-01-24 12:15:52 +0100

    new facebook token

--- const.js
+++ const.js
@@ -7,13 +7,13 @@ if (!WIT_TOKEN) {
 }
 
 // Messenger API parameters
-const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAADBZCLUw5gkBAL9z9XSxDprdwrWdw8p3dXeVfve5DBnIlHtxp142LAicCab8IUZBznAIBccqwazKwyE3SxULfFqGXERZBcVMVBOGE6dridHT58zSjiwZBsVE02rmfZAGtzyb0QXZCceJEqtfykKfZCkG7w2hlGpF3Oe3UkNMPppgZDZD';
+const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAADBZCLUw5gkBAHffQKyUpPKHVsyMDCJGJC0XPTm8bZC5GxU8kI8au1OsLyKFqHc3NKX9MeKBuAwTIHUVsho6u7JLlZB2qTA8IfZA3kWr1ZClvNHvpdG55ptYTPTyjb7kGZAWKiTjTmerRNnLqSmzaWFipX3bvJFP3o87LUZB7jZBQZDZD';
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 if (!FB_VERIFY_TOKEN) {
   FB_VERIFY_TOKEN = "just_do_it";
 }
-// curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAAG2ZBERfJOcBAPqofqH0QH6SAaOsrZAlXE5WSx3fzNeEINb5qMvK2RfrMxkXRSXSOcw1useOkQ83DECZBemDAnt0IclkZBzLUqViiLLmmvLRBUB4zcCk6RWlKoDdnMBzWR1SyGtKpbXN91o7CqdPiuTwCQRZCysPGPyh09HtewZDZD"
+// curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAADBZCLUw5gkBAHffQKyUpPKHVsyMDCJGJC0XPTm8bZC5GxU8kI8au1OsLyKFqHc3NKX9MeKBuAwTIHUVsho6u7JLlZB2qTA8IfZA3kWr1ZClvNHvpdG55ptYTPTyjb7kGZAWKiTjTmerRNnLqSmzaWFipX3bvJFP3o87LUZB7jZBQZDZD"
 
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
